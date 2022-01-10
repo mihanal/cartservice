fdc7ea7f9646386f6f683239c249a1d3e5ccaecd
mihanal <mihan070@gmail.com>
2022-01-10 19:51:33 +0530

    Initial Commit

---
+++ src/main/java/com/micro/MicroCartApplication.java
@@ -0,0 +1,13 @@
+package com.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroCartApplication.class, args);
+	}
+
+}
