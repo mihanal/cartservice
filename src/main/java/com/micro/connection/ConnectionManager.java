357cabd9dad05517cb7501762fcc65df7811167c
mihanal <mihan070@gmail.com>
2022-02-01 22:54:46 +0530

    Removing description

--- src/main/java/com/micro/connection/ConnectionManager.java
+++ src/main/java/com/micro/connection/ConnectionManager.java
@@ -3,7 +3,7 @@ package com.micro.connection;
 public class ConnectionManager {
 
 	public void getConnection() {
-		// TODO Implement the get connection logic
+		// TODO 
 	}
 	
 	public void createJDBCConnection() {
