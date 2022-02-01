fcec2ecfe55932f8c44a7e3a742b779fb0be62b1
mihanal <mihan070@gmail.com>
2022-02-01 11:11:19 +0530

    Adding JDBC connection

--- src/main/java/com/micro/connection/ConnectionManager.java
+++ src/main/java/com/micro/connection/ConnectionManager.java
@@ -5,4 +5,8 @@ public class ConnectionManager {
 	public void getConnection() {
 		// TODO Implement the get connection logic
 	}
+	
+	public void createJDBCConnection() {
+		// TODO Implement jdbc connection
+	}
 }
